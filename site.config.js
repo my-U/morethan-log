7e694daf405e6af85fec2030ce9cca029c678b1f
minyoung <myouth_u@naver.com>
2025-04-23 01:56:22 +0900

    chore: edit profile

--- site.config.js
+++ site.config.js
@@ -13,7 +13,7 @@ const CONFIG = {
   projects: [
     {
       name: `morethan-log`,
-      href: "https://github.com/my-U/morethan-log",
+      href: "",
     },
   ],
   // blog setting (required)
@@ -25,7 +25,7 @@ const CONFIG = {
 
   // CONFIG configration (required)
   link: "https://myouth.vercel.app/",
-  since: 2022, // If leave this empty, current year will be used.
+  since: "", // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
