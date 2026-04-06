405c55c5d1a89b1283a3fdb8e14441a261320f42
minyoung <myouth_u@naver.com>
2026-04-06 22:10:36 +0900

    chore: notion page id 로그 추가

--- src/apis/notion-client/getPosts.ts
+++ src/apis/notion-client/getPosts.ts
@@ -13,6 +13,7 @@ import { TPosts } from "src/types"
 // TODO: react query를 사용해서 처음 불러온 뒤로는 해당데이터만 사용하도록 수정
 export const getPosts = async () => {
   let id = CONFIG.notionConfig.pageId as string
+  console.log("DEBUG: Current Notion Page ID is:", id) // 이 로그를 추가하여 Vercel Build Log에서 확인
   const api = new NotionAPI()
 
   const response = await api.getPage(id)
